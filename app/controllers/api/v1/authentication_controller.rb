ddbe8834c8dac32e1299dc4016e9306cae5c2134
Jorge Gabitto <jgabitto1792@gmail.com>
2021-01-20 12:28:54 -0600

    added before_action authenticate for login

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,8 +1,11 @@
 module Api
     module V1
         class AuthenticationController < ApplicationController
+            before_action :authenticate_user, except: :login
+            
             def login
-                raise AuthenticationError unless user.authenticate(params.require(:password))
+                p params[:email]
+                # raise AuthenticationError unless user.authenticate(params.require(:password))
                 # token = AuthenticationTokenService.call(user.id)
                 # response.set_header('Authentication', token)
                 create_and_send_token(user.id)
